26f9f1824c374f69484cf3b73a532a69c1df54a5
antonio teran <teran@mit.edu>
2019-03-11 17:54:10 -0400

    add sample generation every 25

--- model.py
+++ model.py
@@ -286,7 +286,7 @@ class DCGAN(object):
           % (epoch, config.epoch, idx, batch_idxs,
             time.time() - start_time, errD_fake+errD_real, errG))
 
-        if np.mod(counter, 100) == 1:
+        if np.mod(counter, 20) == 1:
           if config.dataset == 'mnist':
             samples, d_loss, g_loss = self.sess.run(
               [self.sampler, self.d_loss, self.g_loss],
